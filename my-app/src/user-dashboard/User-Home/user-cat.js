3c4bb537335efc7242eecd6be14e306a1e1b32d5
elma <elmakrasniqi16@gmail.com>
2023-09-09 23:24:46 +0200

    user-cat cart

--- my-app/src/user-dashboard/User-Home/user-cat.js
+++ my-app/src/user-dashboard/User-Home/user-cat.js
@@ -12,12 +12,46 @@ var Link = require('react-router-dom').Link
 
 function UserCat(){
    const [pro,setPro]=useState([]);
+   const [Product_id, setProduct_id] = useState("");
+   const [Name, setName] = useState("");
+   const [Description, setDescription] = useState("");
+   const [Price, setPrice] = useState("");
+   const [nr_in_stock, setInStock] = useState("");
+   const [nr_of_stars, setStars] = useState("");
+   const [Price_before_discount, setDiscount] = useState("");
+   const [Category, setCategory] = useState("");
+
 
    const LoadCat=async()=>{
       const response= await axios.get('http://localhost:5000/product/cat');
       console.log(response.data);
       setPro(response.data)
     }
+
+    
+    const handleAddToCart = async (productId) => {
+      const selectedProduct = pro.find((product) => product.Product_id === productId);
+    
+      if (selectedProduct) {
+        const data = {
+          Product_id: selectedProduct.Product_id,
+          Name: selectedProduct.Name,
+          Description: selectedProduct.Description,
+          Price: selectedProduct.Price,
+          nr_in_stock: selectedProduct.nr_in_stock,
+          nr_of_stars: selectedProduct.nr_of_stars,
+          Price_before_discount: selectedProduct.Price_before_discount,
+          Category: selectedProduct.Category,
+        };
+    
+        try {
+          await axios.post('http://localhost:5000/cart', data);
+          window.alert('Product added to cart successfully!');
+        } catch (error) {
+          console.error('Error adding product to cart:', error);
+        }
+      }
+    };
   
     useEffect(()=>{
       LoadCat();
@@ -49,14 +83,16 @@ function UserCat(){
            <i class="FaRegStar"><FaRegStar color="gold" fill="gold" size='18px'/></i>
         </p>
 
-         <h3 class='product_name'>Product name: {product.Name}</h3>
-         <p class="price">{product.Price}$</p>
-         <p class='discount'>number of discount:  {product.Price_before_discount}$</p>
-         <p class='in stock'>number in stock: {product.nr_in_stock}</p>   
+        <h3 class='product_name' value={Name} onChange={(e) => setName(e.target.value)}>Product name: {product.Name}</h3>
+             <p class="price"  value={Price} onChange={(e) => setPrice(e.target.value)}>{product.Price}$</p>
+             <p class='discount'  value={Price_before_discount} onChange={(e) =>  setDiscount(e.target.value)}>number of discount:  {product.Price_before_discount}$</p>
+             <p class='product_name' value={Description} onChange={(e) =>  setDescription(e.target.value)}>description:  {product.Description}</p>
+             <p class='in stock' value={nr_in_stock} onChange={(e) =>  setStars(e.target.value)}>number in stock: {product.nr_in_stock}</p>   
+
        </div>
        <div class='products-button'>
        <motion.button class='purchase'whileHover={{scale:1.1}} >Purchase</motion.button>
-       <motion.button class='add' whileHover={{scale:1.1}}><i class="FaCartPlus"><FaCartPlus  size={'20px'}/></i></motion.button>
+       <motion.button class='add' whileHover={{scale:1.1}}  onClick={() => handleAddToCart(product.Product_id)}><i class="FaCartPlus"><FaCartPlus  size={'20px'}/></i></motion.button>
        </div>
        </div>
       </div> 
