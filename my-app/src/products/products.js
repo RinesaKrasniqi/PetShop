0cd6c5983fd372b58d1ab8a6578eae2aec9e5cbe
MargitaRahimi <mr58700@ubt-uni.net>
2023-09-16 00:05:32 +0200

    added star rating

--- my-app/src/products/products.js
+++ my-app/src/products/products.js
@@ -1,15 +1,11 @@
-import {useRef, useState, useEffect} from 'react';
-import './productscss.css'
-import { FaRegStar } from "react-icons/fa";
-import { FaPaw } from "react-icons/fa";
-import { FaOpencart } from "react-icons/fa";
-import { FaCartPlus } from "react-icons/fa";
-import { FaStar } from "react-icons/fa";
-import {motion} from "framer-motion";
-import  Header from '../Components/header.js';
-import  Footer from '../Components/footer.js';
+import React, { useState, useEffect } from 'react';
+import './productscss.css';
+import { FaCartPlus, FaStar } from "react-icons/fa";
+import { motion } from "framer-motion";
+import Header from '../Components/header.js';
+import Footer from '../Components/footer.js';
 import axios from 'axios';
-var Link = require('react-router-dom').Link
+import { Link } from 'react-router-dom';
 
 function Products() {
    const [pro, setPro] = useState([]);
@@ -26,82 +22,80 @@ function Products() {
       }
    }
 
-   const handleInsert=()=>{
-      const formdata=new FormData();
+   const handleInsert = () => {
+      const formdata = new FormData();
       formdata.append('foto', file);
       axios.post('http://localhost:3000/insert', formdata)
-      .then(res=>console.log(res))
-      .catch(err=>console.log(err));
+         .then(res => console.log(res))
+         .catch(err => console.log(err));
    }
 
    useEffect(() => {
       axios.get('http://localhost:5000/')
-      .then(res => {
-         console.log('API Response:', res); // Log the entire response
-         if (Array.isArray(res.data) && res.data.length > 0) {
-            const i=0;
-            setFoto(res.data[i]);
-         } else {
-            console.error('No data received from the API.');
-         }
-      })
-      .catch(err => console.error('Error fetching data:', err));
+         .then(res => {
+            console.log('API Response:', res); 
+            if (Array.isArray(res.data) && res.data.length > 0) {
+               const i = 0;
+               setFoto(res.data[i]);
+            } else {
+               console.error('No data received from the API.');
+            }
+         })
+         .catch(err => console.error('Error fetching data:', err));
       LoadCat();
    }, []);
 
-   
-
-   console.log(foto.foto);
+   const calculateStarRating = (nr_of_stars) => {
+      return nr_of_stars;
+   }
 
    return (
       <div>
-        <div>
-          <Header />
-        </div>
-  
-        <div className='product-container'>
-          {pro.map((product) => (
-            <div key={product.Product_id} className='card-back'>
-              <div className="card">
-                <div className='fotoja-div'>
-                  <Link to='/shop'>
-                    <img
-                      className='fotoja'
-                      src={`Img/${product.foto}`}
-                      alt={product.Name}
-                    />
-                  </Link>
-                </div>
-  
-                <div className="caption">
-                  <p className="rate">
-                    <FaStar color="gold" fill="gold" size='18px' />
-                    <FaStar color="gold" fill="gold" size='18px' />
-                    <FaStar color="gold" fill="gold" size='18px' />
-                    <FaStar color="gold" fill="gold" size='18px' />
-                    <FaStar color="gold" fill="gold" size='18px' />
-                  </p>
-                  <h3 className='product_name'>Product name: {product.Name}</h3>
-                  <p className="price">{product.Price}$</p>
-                  <p className='discount'>number of discount: {product.Price_before_discount}$</p>
-                  <p className='in stock'>number in stock: {product.nr_in_stock}</p>
-                </div>
-                <div className='products-button'>
-                  <motion.button className='purchase' whileHover={{ scale: 1.1 }}>Purchase</motion.button>
-                  <motion.button className='add' whileHover={{ scale: 1.1 }}>
-                    <i className="FaCartPlus"><FaCartPlus size={'20px'} /></i>
-                  </motion.button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-  
-        <div>
-          <Footer />
-        </div>
+         <div>
+            <Header />
+         </div>
+
+         <div className='product-container'>
+            {pro.map((product) => (
+               <div key={product.Product_id} className='card-back'>
+                  <div className="card">
+                     <div className='fotoja-div'>
+                        <Link to='/shop'>
+                           <img
+                              className='fotoja'
+                              src={`Img/${product.foto}`}
+                              alt={product.Name}
+                           />
+                        </Link>
+                     </div>
+
+                     <div className="caption">
+                        <p className="rate">
+                           {Array.from({ length: calculateStarRating(product.nr_of_stars) }).map((_, index) => (
+                              <FaStar key={index} color="gold" fill="gold" size='18px' />
+                           ))}
+                        </p>
+                        <h3 className='product_name'>Product name: {product.Name}</h3>
+                        <p className="price">{product.Price}$</p>
+                        <p className='discount'>number of discount: {product.Price_before_discount}$</p>
+                        <p className='in stock'>number in stock: {product.nr_in_stock}</p>
+                     </div>
+                     <div className='products-button'>
+                        <motion.button className='purchase' whileHover={{ scale: 1.1 }}>Purchase</motion.button>
+                        <motion.button className='add' whileHover={{ scale: 1.1 }}>
+                           <i className="FaCartPlus"><FaCartPlus size={'20px'} /></i>
+                        </motion.button>
+                     </div>
+                  </div>
+               </div>
+            ))}
+         </div>
+
+         <div>
+            <Footer />
+         </div>
       </div>
-    );
-  }
-  
-  export default Products;
+   );
+}
+
+export default Products;
