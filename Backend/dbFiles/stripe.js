1f455b741cde8d1b72e21af84ed38b34438659f0
MargitaRahimi <mr58700@ubt-uni.net>
2023-09-17 01:31:36 +0200

    started stripe and downloaded stripe

---
+++ Backend/dbFiles/stripe.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'http://localhost:4242/success',
+    cancel_url: 'http://localhost:4242/cancel',
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log(`Listening on port ${4242}!`));
