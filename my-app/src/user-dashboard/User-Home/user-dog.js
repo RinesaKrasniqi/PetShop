f509afac49dcae6a0d54f095afd5f67a02aa71d1
MargitaRahimi <mr58700@ubt-uni.net>
2023-09-18 02:00:35 +0200

    dog fixed

--- my-app/src/user-dashboard/User-Home/user-dog.js
+++ my-app/src/user-dashboard/User-Home/user-dog.js
@@ -1,19 +1,17 @@
-import { useRef, useState, useEffect } from 'react';
+import {useRef, useState, useEffect} from 'react';
+import { FaRegStar } from "react-icons/fa";
+import { FaPaw } from "react-icons/fa";
+import { FaOpencart } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import {motion} from "framer-motion";
+import  UserHeader from './user-header.js';
+import  Footer from '../../Components/footer.js';
 import axios from 'axios';
-import { FaRegStar } from 'react-icons/fa';
-import { FaPaw } from 'react-icons/fa';
-import { FaOpencart } from 'react-icons/fa';
-import { FaCartPlus } from 'react-icons/fa';
-import { FaStar } from 'react-icons/fa';
-import { motion } from 'framer-motion';
-import UserHeader from './user-header.js';
-import Footer from '../../Components/footer.js';
-import { Link } from 'react-router-dom';
-import Cookies from 'js-cookie';
+var Link = require('react-router-dom').Link
 
-<<<<<<< HEAD
-=======
 function UserDog(){
+
    const [pro,setPro]=useState([]);
    const [Product_id, setProduct_id] = useState("");
    const [Name, setName] = useState("");
@@ -24,31 +22,13 @@ function UserDog(){
    const [Price_before_discount, setDiscount] = useState("");
    const [Category, setCategory] = useState("");
    const [foto, setFoto] = useState("");
->>>>>>> b38e02c668d27be91a66bada52355e6fab13c656
+   
+   const LoadDog=async()=>{
+      const response= await axios.get('http://localhost:5000/product/dog');
+      console.log(response.data);
+      setPro(response.data)
+    }
 
-// const setUserIdCookie = (userId) => {
-//   Cookies.set('userId', userId, { expires: 7 });
-// };
-
-<<<<<<< HEAD
-function UserDog() {
-  const [pro, setPro] = useState([]);
-  const [Product_id, setProduct_id] = useState('');
-  const [Name, setName] = useState('');
-  const [Description, setDescription] = useState('');
-  const [Price, setPrice] = useState('');
-  const [nr_in_stock, setInStock] = useState('');
-  const [nr_of_stars, setStars] = useState('');
-  const [Price_before_discount, setDiscount] = useState('');
-  const [Category, setCategory] = useState('');
-  const [Client_id, setClient_id] = useState('');
-
-  const LoadDog = async () => {
-    const response = await axios.get('http://localhost:5000/product/dog');
-    console.log(response.data);
-    setPro(response.data);
-  };
-=======
     const handleAddToCart = async (productId) => {
       const selectedProduct = pro.find((product) => product.Product_id === productId);
     
@@ -77,69 +57,14 @@ function UserDog() {
     const calculateStarRating = (nr_of_stars) => {
       return nr_of_stars;
    }
-    
-      
->>>>>>> b38e02c668d27be91a66bada52355e6fab13c656
   
+  
+    useEffect(()=>{
+      LoadDog();
+    },[]);
 
-  const handleAddToCart = async (productId) => {
-    const selectedProduct = pro.find((product) => product.Product_id === productId);
-  
-    if (selectedProduct) {
-      try {
-        const userId = Cookies.get('Client_id'); 
-        console.log('User ID from cookie:', userId);
-  
-        if (userId) {
-          console.log('User is authenticated with ID:', userId);
-  
-          const data = {
-            Product_id: selectedProduct.Product_id,
-            Name: selectedProduct.Name,
-            Description: selectedProduct.Description,
-            Price: selectedProduct.Price,
-            nr_in_stock: selectedProduct.nr_in_stock,
-            nr_of_stars: selectedProduct.nr_of_stars,
-            Price_before_discount: selectedProduct.Price_before_discount,
-            Category: selectedProduct.Category,
-            Client_id: parseInt(userId)
-          };
-  
-          try {
-            await axios.post('http://localhost:5000/cart', data);
-            window.alert('Product added to cart successfully!');
-          } catch (error) {
-            console.error('Error adding product to cart:', error);
-          }
-        } else {
-          console.log('User is not authenticated');
-          window.alert('Please log in to add products to your cart.');
-        }
-      } catch (error) {
-        console.error('Error getting user ID from cookie:', error);
-      }
-    }
-  };
-  
-
-  useEffect(() => {
-    LoadDog();
-  }, []);
-
-  return (
-    <div>
+    return(
       <div>
-<<<<<<< HEAD
-        <UserHeader />
-      </div>
-      <div className="product-container">
-        {pro.map((product, index) => (
-          <div key={product.Product_id} className="card-back">
-            <div className="card">
-              <div className="fotoja-div">
-                <Link to="/shop">
-                  <img className="fotoja" src="./Img/download.jpg" alt="Product" />
-=======
       <div>
         <UserHeader />
       </div>
@@ -155,29 +80,10 @@ function UserDog() {
                     src={`Img/${product.foto}`}
                     alt={product.Name}
                   />
->>>>>>> b38e02c668d27be91a66bada52355e6fab13c656
                 </Link>
               </div>
 
               <div className="caption">
-<<<<<<< HEAD
-                <p className="rate">
-                  <i className="FaRegStar">
-                    <FaStar color="gold" fill="gold" size="18px" />
-                  </i>
-                  {/* ... (other code) */}
-                </p>
-              </div>
-              <div className="products-button">
-                <motion.button className="purchase" whileHover={{ scale: 1.1 }}>
-                  Purchase
-                </motion.button>
-                <motion.button className="add" whileHover={{ scale: 1.1 }} onClick={() => handleAddToCart(product.Product_id)}>
-                  <i className="FaCartPlus">
-                    <FaCartPlus size={'20px'} />
-                  </i>
-                </motion.button>
-=======
               <p className="rate">
                          {Array.from({ length: calculateStarRating(product.nr_of_stars) }).map((_, index) => (
                             <FaStar key={index} color="gold" fill="gold" size='18px' />
@@ -192,27 +98,17 @@ function UserDog() {
               <div className='products-button'>
                 <motion.button className='purchase' whileHover={{ scale: 1.1 }}>Purchase</motion.button>
                 <motion.button class='add' whileHover={{scale:1.1}}  onClick={() => handleAddToCart(product.Product_id)}><i class="FaCartPlus"><FaCartPlus  size={'20px'}/></i></motion.button>
->>>>>>> b38e02c668d27be91a66bada52355e6fab13c656
               </div>
             </div>
           </div>
         ))}
       </div>
-<<<<<<< HEAD
-=======
 
->>>>>>> b38e02c668d27be91a66bada52355e6fab13c656
       <div>
         <Footer />
       </div>
     </div>
-<<<<<<< HEAD
-  );
+    );
 }
-=======
-);
-
-                           }
->>>>>>> b38e02c668d27be91a66bada52355e6fab13c656
 
 export default UserDog;
