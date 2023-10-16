1ad4c1dc1d0d19f89dd6f536f50a32274fbdea31
RinesaKrasniqi <rk56179@ubt-uni.net>
2023-10-16 13:38:43 +0200

    commit

--- Backend/dbFiles/dbConfig.js
+++ Backend/dbFiles/dbConfig.js
@@ -1,7 +1,7 @@
 var config={
-    user:"Margita",
-    password: "123",
-    server: "MARGITA1083\\MSSQLSERVER01",
+    user:"Rinesa",
+    password: "1234",
+    server: "DISCORD\\SQLEXPRESS",
     database : "ProjektiLAB1",
     driver: "msnodesqlv8",
     options:{
